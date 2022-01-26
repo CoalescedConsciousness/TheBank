319f6c5ba40f198921d5b8d11c018c4e377ec886
Santi <Santi@LAPTOP-O3EPNBL4>
2022-01-26 08:33:55 +0100

    Add project files.

---
+++ ConsoleApp1/Bank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank1
+{
+    internal class Bank
+    {
+        public string bankName { get; set; }
+        public string account { get; set; }
+    }
+}
